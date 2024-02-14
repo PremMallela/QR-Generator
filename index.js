691a30b81a19af37e3c495a6c438fba63d5d7ef8
PremMallela <86968287+PremMallela@users.noreply.github.com>
2024-02-14 19:47:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
+//1. Use the inquirer npm package to get user input.
 inquirer
 .prompt([
     {
@@ -10,10 +11,9 @@ inquirer
 ])
 .then((answers) => {
   const url = answers.URL;
-  var qr_svg = qr.image(url);
+  var qr_svg = qr.image(url);  //2. Use the qr-image npm package to turn the user entered URL into a QR code image.
   qr_svg.pipe(fs.createWriteStream('qr_image.png'));
-
-  fs.writeFile('message.txt', url, (err) => {
+  fs.writeFile('message.txt', url, (err) => {   //3. Create a txt file to save the user input using the native fs node module.
     if (err) throw err;
     console.log('The file has been saved!');
   }); 
@@ -26,8 +26,3 @@ inquirer
   }
 });
 
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
